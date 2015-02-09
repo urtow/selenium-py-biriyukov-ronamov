cc024705ecbc53d88a83583a76c901e089f05f9d
urtow <desruptorrogue@gmail.com>
2015-02-09 21:27:08 +0300

    Fix search

--- runTest.py
+++ runTest.py
@@ -1,18 +1,20 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-from time import sleep
-# from selenium.webdriver.common.by import By
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 # from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions import NoSuchElementException
+# from selenium.common.exceptions import NoSuchElementException
 # from selenium.common.exceptions import NoAlertPresentException
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support.expected_conditions import visibility_of_element_located
 
 
-class TestApi():
+class TestAfterLoginApi():
 
     @classmethod
     def setup_class(cls):
         cls.driver = webdriver.Firefox()
+        cls.wait = WebDriverWait(cls.driver, 10)
         cls.driver.implicitly_wait(5)
         cls.base_url = "http://lamp.local/"
         cls.driver.get(cls.base_url)
@@ -24,7 +26,10 @@ class TestApi():
         cls.driver.find_element_by_name("submit").click()
 
     def test_search_success(self):
+        # Wait while we see all
+        self.wait.until(visibility_of_element_located((By.ID, 'results')))
         driver = self.driver
+        driver.get(self.base_url)
         movie_title = "yaya"
         driver.find_element_by_id("q").clear()
         driver.find_element_by_id("q").send_keys(movie_title)
@@ -37,32 +42,24 @@ class TestApi():
         driver.find_element_by_id("q").clear()
         driver.find_element_by_id("q").send_keys(movie_title)
         driver.find_element_by_id("q").send_keys(Keys.RETURN)
-        assert not self.is_movie_present(movie_title)
+        assert self.no_search_results()
 
     def no_search_results(self):
-        # magic
-        sleep(0.2)
-        try:
-            result = self.driver.find_element_by_id('results')
-            message = result.find_elements_by_class_name("content")
-            if message.text == 'No movies where found.':
-                return True
-            else:
-                return False
-        except NoSuchElementException:
+        results = self.wait.until(visibility_of_element_located((By.ID, 'results')))
+        message = results.find_element_by_class_name("content")
+        if message.text == 'No movies where found.':
+            return True
+        else:
+            return False
             return False
 
     def is_movie_present(self, movie_title):
-        # magic
-        sleep(0.2)
-        try:
-            result = self.driver.find_element_by_id('results')
-            movies_titles = result.find_elements_by_class_name("title")
-            for title in movies_titles:
-                if title.text == movie_title:
-                    return True
-        except NoSuchElementException:
-            return False
+        results = self.wait.until(visibility_of_element_located((By.ID, 'results')))
+        movies_titles = results.find_elements_by_class_name("title")
+        for title in movies_titles:
+            if title.text == movie_title:
+                return True
+        return False
 
     @classmethod
     def teardown_class(cls):
